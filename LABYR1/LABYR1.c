11fb1053aa2bb520270a53166a7a0e825adf9cbb
arthurmlv <arthurmv123@gmail.com>
2023-09-20 21:06:43 -0300

    refactor: ajustes labyr1

--- LABYR1/LABYR1.c
+++ LABYR1/LABYR1.c
@@ -7,34 +7,28 @@ int visited[1005][1005];
 int coluna, linha;
 int sinkx=0, sinky=0;
 
-void zeravisita() {
-    for(int i=0;i<1001;++i)
-        for(int j=0;j<1001;++j)
-            visited[i][j] = 0;
-}
-
-void DFS(int q, int w, int max){
-    visited[q][w]=1;
+void DFS(int q, int w, int max, int p){
+    visited[q][w]++;
     int count = 0;
     if(matriz[q][w]=='.' && q<linha && w<coluna && q>-1 && w>-1){
-        if(matriz[q+1][w]=='.' && q<(linha-1) && visited[q+1][w]==0){
+        if(matriz[q+1][w]=='.' && visited[q+1][w]==p){
             count++;
-            DFS(q+1, w, max+1);
+            DFS(q+1, w, max+1, p);
         }
-        if(matriz[q-1][w]=='.' && q>0 && visited[q-1][w]==0){
+        if(matriz[q-1][w]=='.' && visited[q-1][w]==p){
             count++;
-            DFS(q-1, w, max+1);
+            DFS(q-1, w, max+1, p);
         }
-        if(matriz[q][w+1]=='.' && w<(coluna-1) && visited[q][w+1]==0){
+        if(matriz[q][w+1]=='.' && visited[q][w+1]==p){
             count++;
-            DFS(q, w+1, max+1);
+            DFS(q, w+1, max+1, p);
         }
-        if(matriz[q][w-1]=='.' && w>0 && visited[q][w-1]==0){
+        if(matriz[q][w-1]=='.' && visited[q][w-1]==p){
             count++;
-            DFS(q, w-1, max+1);
+            DFS(q, w-1, max+1, p);
         }
         if(count!=0) max++;
-    } else return;
+    }
     if(max>tamanho){
         tamanho = max;
         sinkx = q;
@@ -57,13 +51,12 @@ int main(){
                 if(matriz[j][k]=='.'){
                     inix = j;
                     iniy = k;
+                    visited[j][k] = 0;
                 }
             }
         }
-        DFS(inix, iniy, 0);
-        zeravisita();
-        DFS(sinkx, sinky, 0);
-        zeravisita();
+        DFS(inix, iniy, 0, 0);
+        DFS(sinkx, sinky, 0, 1);
         printf("Maximum rope length is %d.\n", tamanho);
     }
     return 0;
